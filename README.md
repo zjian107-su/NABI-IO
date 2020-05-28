0a6e3874a6ccb00ca7a243f5068b90ffc5f651a6
Zezheng Jiang <danieljc8888@gmail.com>
2020-05-28 12:36:26 -0400

    add some emojis

--- README.md
+++ README.md
@@ -10,5 +10,6 @@ A Gatsby website that helps people to raise a lovely first pup. #PersonalProject
 ## **Change Log**
 
 ```
-  - 0527: Initiate the web dev. Starting as "Hello World" and then improving while learning on "Gatsby Tutorial and Projects Course".
+  - 0527: Initiate the web dev. Starting as "Hello World" and then improving while learning on "Gatsby Tutorial and Projects Course". The home page has not been reset, but added two RCC(react class component) of /home_page and /intro
+  - 0528: Adding emojis into the structure. Imrpove home page by readesigning it and adding LINK components and Navbar component.  
 ```
--- nabi-io/src/pages/index.js
+++ nabi-io/src/pages/index.js
@@ -1,20 +1,28 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
+import Emoji from "../components/emoji"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1
+      style={{
+        fontWeight: `bold`,
+      }}
+    >
+      🐶欢迎来到狗狗的世界
+    </h1>
+    <h2>训练一条听话的对世界充满爱的狗狗</h2>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <Link to="/intro">1. Intro</Link>
+    <div> </div>
+    <Link to="/home_page">2. Home Page</Link>
   </Layout>
 )
 
